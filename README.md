16bbff09a0fecce6a2efb216e51b16263a0dd552
tomoyachen <tomoya_chen@163.com>
2021-11-21 01:45:06 +0800

    feat(): first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# CSDN BLOG EXPORT
+
+> 导出模板默认带上了作者信息、版权申明、源地址、文章分类。*希望大家尊重原创，不要用于奇怪的用途。*
+
+心血来潮写了个 CSDN 博客导出为 Markdown 格式的工具
+
+btw 本来以为要自己写映射，发现有比较成熟的 [html2md](https://github.com/Alir3z4/html2text)  库了，太好了。
+
+
+# 说明
+## 安装依赖
+```bash
+poetry install
+```
+
+## 常规使用
+```bash
+直接修改 main 入口的源码后执行吧。
+参数来源均可以从文章详情页获取：/username/article/details/123456789
+
+# 导出单篇
+export_article(username, article_id)
+
+# 导出全部
+export_all_article(username)
+```
+
+## 命令行使用（后期更新，但...真的需要吗？）
+```bash
+# 非虚拟环境下
+poetry run python export.py username 123456789
+
+# 进入虚拟环境后执行
+poetry shell
+python export.py username
+```
+
+## 关于模板
+目前模板维护在 html 中，我知道维护在 md 中更合理，但是我偷懒了。
+
+html 中的图片基本都来自于 CSDN 图床，建议有需求的小伙伴，自己替换成自己的图床或下载为本地图片。
+
+## 已知问题
+1. image or link 地址过长时可能会换行，已经做了一些强兼容，但可能还会遇到新的情况
+2. html 中 * 星号，转成 markdown 后，一定会识别成列表，因为没有 +/ 转译。
+3. html 中 二级 li 标签，转成 markdown 后，有概率变成多个 * 号。
+ 
+\* 应该不会去修改 html2md 的源码了，毕竟精力有限，并且影响不大。
---
+++ app.py
@@ -0,0 +1,155 @@
+from bs4 import BeautifulSoup
+import requests
+from fake_useragent import UserAgent
+import lxml
+import html2text
+import os
+
+HOST = "https://blog.csdn.net"
+USER_AGENT = UserAgent().random
+requests.packages.urllib3.disable_warnings()
+
+
+def get_article_list_part_with_page(username: str, page: int = 1) -> dict:
+    url = f'{HOST}/community/home-api/v1/get-business-list'
+    headers = {
+        'referer': f'{HOST}/{username}?type=blog',
+        'User-Agent': USER_AGENT,
+        'accept': 'application/json, text/plain, */*'
+    }
+    params = {
+        "username": username,
+        "businessType": 'blog',
+        "orderBy": "",
+        "noMore": False,
+        "page": page,
+        "size": 20
+    }
+    respone = requests.get(url=url, params=params, headers=headers)
+    return respone.json()
+
+
+def get_all_article_list(username: str) -> list:
+    part = get_article_list_part_with_page(username)
+    article_amount: int = part['data']['total']
+    article_list: list = part['data']['list']
+    i = 1
+    while article_amount - 20 > 0:
+        article_amount -= 20
+        i += 1
+        next_part = get_article_list_part_with_page(username, i)
+        article_list += next_part['data']['list']
+    return article_list
+
+
+def visit_page(url: str) -> BeautifulSoup:
+    headers = {
+        'referer': HOST,
+        'User-Agent': USER_AGENT,
+        'accept': "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+    }
+    response = requests.get(url=url, headers=headers)
+    response.encoding = 'utf-8'
+    html_content = response.text
+    soup = BeautifulSoup(html_content, 'lxml')
+    return soup
+
+
+def get_article_html(username: str, article_id: int, needTOC: bool = True) -> str:
+    url = f'{HOST}/{username}/article/details/{article_id}'
+    article_detail = visit_page(url)
+
+    title = article_detail.select_one('h1#articleContentId')
+    author = article_detail.select_one('.profile-box')
+    copyright = article_detail.select_one('.slide-content-box')
+    tags = article_detail.select_one('div.blog-tags-box')
+    content = article_detail.select_one('div#article_content')
+
+    html = f"""{title}
+
+    {tags}
+
+    {copyright}
+
+    {'[TOC]' if needTOC else ''}
+
+    {content}
+    """
+
+    return html
+
+
+def get_article_markdown(article_html: str) -> str:
+    md = html2text.html2text(article_html)
+    # 兼容 html2text 问题
+    # 1. image or link 地址过长时可能会换行，已经做了一些强兼容，但可能还会遇到新的情况
+    md = md.replace('x-oss-\nprocess=image/watermark', 'x-oss-process=image/watermark')
+    md = md.replace('/licenses/by-\nsa', '/licenses/by-sa')
+    md = md.replace('/img-\nblog.csdn.net', '/img-blog.csdn.net')
+    # 仍存在的问题：
+    # 1. html 中 * 星号，转成 markdown 后，一定会识别成列表，因为没有 +/ 转译。
+    # 2. html 中 二级 li 标签，转成 markdown 后，有概率变成多个 * 号。
+    return md
+
+
+def export(file_dir, content):
+    if not os.path.exists(os.path.dirname(file_dir)):
+        os.makedirs(os.path.dirname(file_dir))
+    with open(file_dir, 'w', encoding='utf8') as f:
+        f.write(content)
+
+
+def get_title_from_article_html(article_html: str):
+    soup = BeautifulSoup(article_html, 'lxml')
+    article_filename = soup.select_one('h1#articleContentId').text  # 暂时这么写吧..
+    article_filename = article_filename.replace('/', '／').replace(':', '：').replace('*', '＊').replace('?', '？').replace(
+        '#', '＃')
+    return article_filename
+
+
+def export_article(username, article_id: str):
+    '''
+    导出单篇文章
+    :param username: 博主用户名
+    :param article_id: 文章 id
+    :return:
+    '''
+    article_html = get_article_html(username=username, article_id=article_id, needTOC=True)
+    article_markdown = get_article_markdown(article_html)
+
+    try:
+        article_filename = get_title_from_article_html(article_html)
+        export(f'./articles/{username}/{article_filename}.md', article_markdown)
+        print(f'《{article_filename}》 导出成功')
+    except Exception as e:
+        print(f'《{article_filename}》 导出失败', e)
+
+
+def export_all_article(username: str, index: int = 0):
+    '''
+    导出全部文章，支持指定列表起始索引
+    :param username: 博主用户名
+    :param index: 起始索引，默认 0 第一篇
+    :return:
+    '''
+    article_list = get_all_article_list(username)
+    i = 0
+    for article in article_list[index:]:
+        article_id = article['articleId']
+        article_html = get_article_html(username=username, article_id=article_id, needTOC=True)
+        article_markdown = get_article_markdown(article_html)
+        # print(article_markdown)
+
+        try:
+            i += 1
+            article_filename = get_title_from_article_html(article_html)
+            export(f'./articles/{username}/{article_filename}.md', article_markdown)
+            print(f'第 {i} 篇 《{article_filename}》 导出成功')
+        except Exception as e:
+            print(f'第 {i} 篇 《{article_filename}》 导出失败', e)
+
+
+if __name__ == "__main__":
+    username = 'username'
+    export_article(username=username, article_id=123456789)
+    export_all_article(username=username, index=0)
